e55718a9bdbc0bdc90923bb2c18a0d08b63d1fda
David Schnoll <davidschnoll@gmail.com>
2024-01-31 00:51:05 +0200

    Initial commit

---
+++ marines/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MarinesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'marines'
---
+++ planet_economy/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PlanetEconomyConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'planet_economy'
