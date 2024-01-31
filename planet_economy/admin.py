7a6d18548eae50bd4dbaf9617be70c75622b25d4
David Schnoll <davidschnoll@gmail.com>
2024-01-31 02:19:51 +0200

    Create Planet, Ware and PlanetWare models

--- planet_economy/admin.py
+++ planet_economy/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from planet_economy.models import Planet, PlanetWare, Ware
 
-# Register your models here.
+
+@admin.register(Planet)
+class PlanetAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PlanetWare)
+class PlanetWareAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Ware)
+class WareAdmin(admin.ModelAdmin):
+    pass
--- planet_economy/apps.py
+++ planet_economy/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PlanetEconomyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'planet_economy'
+    verbose_name = 'Planetary Economy'
