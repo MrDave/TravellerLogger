cb8ab453d4e5bd2e204e9771ee62cab0b93cb3d1
David Schnoll <davidschnoll@gmail.com>
2024-02-01 02:28:44 +0200

    Create basic planet details page

--- planet_economy/urls.py
+++ planet_economy/urls.py
@@ -4,5 +4,6 @@ from planet_economy import views
 app_name = "planet_economy"
 
 urlpatterns = [
-    path("", views.planet_list)
+    path("<int:planet_id>", views.planet_details, name="planet_details"),
+    path("", views.planet_list, name="planet_list")
 ]
--- planet_economy/views.py
+++ planet_economy/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from planet_economy.models import Planet
+from django.http import JsonResponse
 
 
 def planet_list(request):
@@ -8,3 +9,13 @@ def planet_list(request):
         "planets": planets
     }
     return render(request, "planets.html", context)
+
+
+def planet_details(request, planet_id):
+    planet = get_object_or_404(Planet, planet_id=planet_id)
+    payload = {
+        "name": planet.name,
+        "planet_id": planet.planet_id,
+        "wiki_link": planet.wiki_link
+    }
+    return JsonResponse(payload)
