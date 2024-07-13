b0dcc1e371acff758b14b9c8010940b32af8b0d0
David Schnoll <davidschnoll@gmail.com>
2024-07-13 20:31:16 +0300

    Sample index and "planet_list" views

--- planets/models.py
+++ planets/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Planet(models.Model):
     name = models.CharField(max_length=20, verbose_name="planet name")
     planet_id = models.CharField(max_length=4, blank=True)
-    wiki_link = models.TextField()
+    wiki_link = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
--- planets/views.py
+++ planets/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from planets.models import Planet
+from django.http import JsonResponse
 
-# Create your views here.
+
+def planet_list(request):
+    planets = Planet.objects.all()
+    response = {
+        "planets": [{"name": planet.name, "planet_id": planet.planet_id, "wiki_link": planet.wiki_link} for planet in planets]
+    }
+
+    return JsonResponse(response, json_dumps_params={"ensure_ascii": False, "indent": 4})
+
+
+def index(request):
+    return JsonResponse({"Welcome": "to Traveller Logger"})
+
