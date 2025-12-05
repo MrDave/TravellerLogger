2bda5bb1f5d27e2163a1ac63ea3cd6e6ae540f3d
David Schnoll <davidschnoll@gmail.com>
2025-12-05 13:09:14 +0200

    Create Campaign and LogEntry models

--- space_calendar/admin.py
+++ space_calendar/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from space_calendar.models import Campaign, GameDate, LogEntry
 
-# Register your models here.
+
+@admin.register(Campaign)
+class CampaignAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(GameDate)
+class GameDateAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    pass
--- space_calendar/models.py
+++ space_calendar/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class GameDate(models.Model):
@@ -14,6 +15,37 @@ class GameDate(models.Model):
                 violation_error_message="days must be in range 1-365",
             ),
         ]
+        ordering = ["year", "day_of_the_year"]
 
     def __str__(self):
-        return f"{self.year} - {self.day_of_the_year:03d}"
+        return f"{self.day_of_the_year:03d}-{self.year} IC"
+
+
+class Campaign(models.Model):
+    owner = models.ForeignKey(User, related_name="campaigns", on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    current_date = models.ForeignKey(GameDate, related_name="campaigns", on_delete=models.PROTECT)
+
+    class Meta:
+        ordering = ["owner", "name"]
+
+    def __str__(self):
+        return self.name
+
+
+class LogEntry(models.Model):
+    campaign = models.ForeignKey(Campaign, related_name="log_entries", on_delete=models.CASCADE)
+    game_date = models.ForeignKey(GameDate, related_name="log_entries", on_delete=models.CASCADE)
+    text = models.TextField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["campaign", "game_date"],
+                name="unique_log_for_date",
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.campaign} - {self.game_date}"
+
