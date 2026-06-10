6a1bcb721c219370e86cbcfa3ad9f5798634ff2f
David Schnoll <davidschnoll@gmail.com>
2026-06-10 22:24:09 +0300

    Create a PlanetDeatil view

--- planets/views.py
+++ planets/views.py
@@ -1,7 +1,16 @@
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 from planets.models import Planet
 
 
 class PlanetListView(ListView):
     model = Planet
     context_object_name = "planet_list"
+
+
+class PlanetDetailView(DetailView):
+    model = Planet
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # TODO: add PlanetWares context
+        return context
